cd02a4592a7b44f6775341ce8841044aef230eb1
PhsTutors <phs3@discente.ifpe.edu.br>
2023-02-16 19:55:17 -0300

    Add Arquivos do Vue

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import home from '../components/home.vue'
+import cadastro from '../components/cadastro.vue'
+import login from '../components/Login.vue'
+import inscricaoProjeto from '../components/inscricaoProjeto.vue'
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/cadastro',
+    name: 'cadastro',
+    component: cadastro
+  },
+  {
+    path: '/inscricaoProjeto',
+    name: 'inscricaoProjeto',
+    component: inscricaoProjeto
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
